7f66272e853e2218e5c01cff82ea1a3bb6637a30
jeetkanodia <jeetkanodia@gmail.com>
2023-08-03 21:59:24 +0530

    inital frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Task Manager</h1>
+        <div className="top">
+          <input type="text" placeholder="Task Title"/>
+          <input type="text" placeholder="Task Description"/>
+          
+          <div className="add">Add Task</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
